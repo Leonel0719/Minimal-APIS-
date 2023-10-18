d5e8fe6bcb024e030af5341e42587d46a2b036df
Leonel Alexander <leo.gise02@outlook.com>
2023-10-17 20:18:57 -0600

    Materias

---
+++ Minimal API 2/LADCH20230905Materias/LADCH20230905Materias/Controllers/MateriaController.cs
@@ -0,0 +1,42 @@
+﻿using LADCH20230905Materias.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LADCH20230905Materias.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MateriaController : ControllerBase
+    {
+        static List<Materia> materias = new List<Materia>();
+
+        [HttpGet]
+        public IEnumerable<Materia> Get()
+        {
+            return materias;
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var exiatingMateria = materias.FirstOrDefault(m => m.Id == id);
+            if (exiatingMateria != null)
+            {
+                materias.Remove(exiatingMateria);
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Materia materia)
+        {
+            materias.Add(materia);
+            return Ok();
+        }
+    }
+}
---
+++ Minimal API 2/LADCH20230905Materias/LADCH20230905Materias/Models/Materia.cs
@@ -0,0 +1,8 @@
+﻿namespace LADCH20230905Materias.Models
+{
+    public class Materia
+    {
+        public int Id { get; set; }
+        public string materia {  get; set; }
+    }
+}
