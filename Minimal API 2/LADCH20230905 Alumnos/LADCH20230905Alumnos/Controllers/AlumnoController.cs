43e5e5707f7ba6ad6a2cb8d88bc9de4a571a4a66
Leonel Alexander <leo.gise02@outlook.com>
2023-10-17 20:11:37 -0600

    2

---
+++ Minimal API 2/LADCH20230905 Alumnos/LADCH20230905Alumnos/Controllers/AlumnoController.cs
@@ -0,0 +1,70 @@
+﻿using LADCH20230905Alumnos.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LADCH20230905Alumnos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlumnoController : ControllerBase
+    {
+        static List<Alumno> alumnos = new List<Alumno>();
+
+        //agregar
+        [HttpPost]
+        public IActionResult Post([FromBody] Alumno alumno)
+        {
+            alumnos.Add(alumno);
+            return Ok();
+        }
+
+        // modificar
+        [HttpPut("{id}")]
+        public IActionResult Put    (int id, [FromBody] Alumno alumno)
+        {
+            var existingAlumno = alumnos.FirstOrDefault(a => a.Id == id);
+            if (existingAlumno != null)
+            {
+                existingAlumno.nombre = alumno.nombre;
+                existingAlumno.apellido = alumno.apellido;
+                existingAlumno.edad = alumno.edad;
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var existingAlumno = alumnos.FirstOrDefault(a => a.Id == id);
+            if (existingAlumno != null)
+            {
+                alumnos.Remove(existingAlumno);
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpGet]
+        public IEnumerable<Alumno> Get()
+        {
+            return alumnos;
+        }
+
+        [HttpGet("{id}")]
+        public Alumno Get(int id)
+        {
+            var alumno = alumnos.FirstOrDefault(c => c.Id == id);
+            return alumno;
+        }
+
+    }
+}
---
+++ Minimal API 2/LADCH20230905 Alumnos/LADCH20230905Alumnos/Models/Alumno.cs
@@ -0,0 +1,10 @@
+﻿namespace LADCH20230905Alumnos.Models
+{
+    public class Alumno
+    {
+        public int Id { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string edad { get; set; }
+    }
+}
