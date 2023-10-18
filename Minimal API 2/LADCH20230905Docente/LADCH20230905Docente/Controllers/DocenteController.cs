515c5b7ce9e70ad083d9c08c5bdb7b38d33f39fd
Leonel Alexander <leo.gise02@outlook.com>
2023-10-17 20:17:02 -0600

    Docentes

---
+++ Minimal API 2/LADCH20230905Docente/LADCH20230905Docente/Controllers/DocenteController.cs
@@ -0,0 +1,31 @@
+﻿using LADCH20230905Docente.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LADCH20230905Docente.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DocenteController : ControllerBase
+    {
+        static List<Docente> docentes = new List<Docente>();
+
+        [HttpPost]
+        // agrecar
+        public IActionResult Post([FromBody] Docente docente)
+        {
+            docentes.Add(docente);
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+
+        public Docente Get(int id)
+        {
+            var docente = docentes.FirstOrDefault(d => d.Id == id);
+            return docente;
+        }
+
+    }
+}
---
+++ Minimal API 2/LADCH20230905Docente/LADCH20230905Docente/Models/Docente.cs
@@ -0,0 +1,9 @@
+﻿namespace LADCH20230905Docente.Models
+{
+    public class Docente
+    {
+        public int Id { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+    }
+}
