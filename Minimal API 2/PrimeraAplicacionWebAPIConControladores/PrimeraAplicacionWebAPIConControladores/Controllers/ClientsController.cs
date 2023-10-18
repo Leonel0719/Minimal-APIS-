40b0a5774bb5864fac060b24856243d04518a20a
Leonel Alexander <leo.gise02@outlook.com>
2023-10-17 20:21:47 -0600

    API controlador

---
+++ Minimal API 2/PrimeraAplicacionWebAPIConControladores/PrimeraAplicacionWebAPIConControladores/Controllers/ClientsController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using PrimeraAplicacionWebAPIConControladores.Models;
+using System.Data.SqlTypes;
+using System.Security.Cryptography;
+
+namespace PrimeraAplicacionWebAPIConControladores.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+        static List<Client> clients = new List<Client>();
+
+        [HttpGet]
+        public IEnumerable<Client> Get()
+        {
+            return clients;
+        }
+
+        [HttpGet("(id)")]
+        public Client Get(int id)
+        {
+            var client = clients.FirstOrDefault(c => c.Id == id);
+            return client;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Client client)
+        {
+            clients.Add(client);
+            return Ok();  
+        }
+
+        [HttpPut("(id)")]
+        public IActionResult Put(int id, [FromBody] Client client)
+        {
+            var existingClient = clients.FirstOrDefault(c => c.Id == id);
+            if (existingClient != null)
+            {
+                existingClient.Name = client.Name;
+                existingClient.LastName = client.LastName;
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpDelete("{id}")]
+
+        public IActionResult Delete(int id)
+        {
+            var existingClient = clients.FirstOrDefault(c => c.Id == id);
+            if (existingClient != null)
+            {
+                clients.Remove(existingClient);
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+        
+    }
+}
---
+++ Minimal API 2/PrimeraAplicacionWebAPIConControladores/PrimeraAplicacionWebAPIConControladores/Models/Client.cs
@@ -0,0 +1,9 @@
+﻿namespace PrimeraAplicacionWebAPIConControladores.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+    }
+}
